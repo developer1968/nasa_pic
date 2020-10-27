8b39f8dd9560319bc1b2d45990ff26d5c1239f27
developer1968 <73335340+developer1968@users.noreply.github.com>
2020-10-27 15:35:48 +0100

    Add files via upload

---
+++ src/test/java/com/example/nasapicture/NasaPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasapicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaPictureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
