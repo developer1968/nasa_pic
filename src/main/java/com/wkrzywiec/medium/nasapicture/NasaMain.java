bb04714e2ece34a130f6466e964b6ef652d384a2
svil1 <svil1@blabla.com>
2020-10-28 12:10:43 +0100

    sdsa

---
+++ src/main/java/com/wkrzywiec/medium/nasapicture/NasaMain.java
@@ -0,0 +1,14 @@
+package com.wkrzywiec.medium.nasapicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaMain.class, args); 
+
+	}
+
+}
